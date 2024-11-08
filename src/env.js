6fcfb5572e57b7126cd656fd79d7082ce7284fee
Vilhelm Toivonen <vilhelm.toivonen@gmail.com>
2024-11-08 23:32:46 +0200

    added sms worker

--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    SMS_USERNAME: z.string(),
+    SMS_PASSWORD: z.string(),
   },
 
   /**
@@ -29,6 +31,8 @@ export const env = createEnv({
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
+    SMS_USERNAME: process.env.SMS_USERNAME,
+    SMS_PASSWORD: process.env.SMS_PASSWORD,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
