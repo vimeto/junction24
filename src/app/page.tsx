c0032f2e636698f7962590f64b1870520efe26e9
Oskari Aalto <heikki.oskari.aalto@gmail.com>
2024-11-09 12:30:05 +0200

    created new home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { getMyImages } from "~/server/queries";
+import { Card, CardContent } from "~/components/ui/card";
 
 export const dynamic = "force-dynamic";
 
@@ -31,34 +32,43 @@ async function Images() {
 export default async function HomePage() {
   return (
     <main className="">
-      <SignedOut>
-        <div className="h-full w-full text-center text-2xl">
-          Please sign in above
-        </div>
-      </SignedOut>
-      <SignedIn>
-        <div className="relative">
-          <Images />
-          <Link
-            href="/audits/1"
-            className="fixed left-1/2 top-1/2 inline-flex h-12 w-12 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-primary text-primary-foreground shadow-lg hover:bg-primary/90"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
-            </svg>
-          </Link>
-        </div>
-      </SignedIn>
+      <div className="grid h-screen w-screen grid-cols-1 items-center justify-center gap-6 p-4 sm:grid-cols-2 sm:gap-2">
+        <Link
+          href="/admin"
+          className="flex h-full w-full items-end justify-center sm:items-center"
+        >
+          <Card className="h-full max-h-60 w-full max-w-60 rounded-[20px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-2 shadow-2xl hover:bg-neutral-900 sm:max-h-80 sm:max-w-xs">
+            <CardContent className="flex h-full flex-col items-center justify-center rounded-2xl bg-neutral-800">
+              <div className="items-center justify-center rounded-2xl">
+                <h2 className="flex items-center justify-center pb-1 text-xl sm:text-4xl">
+                  Admin demo
+                </h2>
+                <span className="sm:text-md flex items-center justify-center text-sm text-neutral-300">
+                  (Requires login)
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+
+        <Link
+          href="/audits/1"
+          className="flex h-full w-full items-start justify-center sm:items-center"
+        >
+          <Card className="h-full max-h-60 w-full max-w-60 rounded-[20px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-2 shadow-2xl hover:bg-neutral-900 sm:max-h-80 sm:max-w-xs">
+            <CardContent className="flex h-full flex-col items-center justify-center rounded-2xl bg-neutral-800">
+              <div className="items-center justify-center">
+                <h2 className="flex items-center justify-center pb-1 text-xl sm:text-4xl">
+                  Audit demo
+                </h2>
+                <span className="sm:text-md flex items-center justify-center text-sm text-neutral-300">
+                  (Does not require login)
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default {
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
+  			sm: 'calc(var(--radius) - 4px)',
   		},
   		colors: {
   			background: 'hsl(var(--background))',
